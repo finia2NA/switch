33b925782983dce8f0fe9cde47998ac2d7fcc2ae
Sanyam G <sanyamg2006@gmail.com>
2026-03-12 22:41:31 -0500

    wip — picking up menu bar items, need to filter

--- Sources/Switch/WindowEnumerator.swift
+++ Sources/Switch/WindowEnumerator.swift
@@ -16,6 +16,7 @@ enum WindowEnumerator {
             guard let id = dict[kCGWindowNumber as String] as? CGWindowID,
                   let pid = dict[kCGWindowOwnerPID as String] as? pid_t else { return nil }
             let title = (dict[kCGWindowName as String] as? String) ?? ""
+            // FIXME: getting menu bar items + Dock + system overlays in here
             return WindowInfo(id: id, title: title, pid: pid)
         }
     }
